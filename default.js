894d36441a3c02d0a433df761cbd45918f4853d6
how2945ard <how2945ard@gmail.com>
2016-05-11 13:24:03 +0800

    fix const

--- default.js
+++ default.js
@@ -40,6 +40,7 @@ function random(n) { // 從 0 到 n-1 中選一個亂數
 function getAnswer(say) {
   if (qaList.length <= 4 || updatedQuestions) {
     Question.find({}, function(err, docs) {
+      qaList = [];
       updatedQuestions = false;
       qaList.push.apply(defualtQaList, docs)
       console.log(qaList)
